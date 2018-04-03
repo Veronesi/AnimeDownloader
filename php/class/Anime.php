32b4ec438b8998ae28cb6080475780050f2ba197
Facundo Veronesi <veronesif@frcu.utn.edu.ar>
2018-04-02 22:49:07 -0300

    agregue el encabezado

--- php/class/Anime.php
+++ php/class/Anime.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * PHP version 7
+ * @author      @Fanaes.
+ * @copyright   2017-2018 Free SoftWare.
+ * @version     1.0.1
+ * @link        https://github.com/Veronesi/AnimeDownloader
+ * @since       12/01/2017
+ */
+$servername = "localhost";
+$username = "root";
+$password = "123456789";
+$conn = new mysqli($servername, $username, $password);
+
 class Anime{
 	PUBLIC $Nombre;
 	PUBLIC $Capitulo = array();
@@ -9,11 +22,26 @@ class Anime{
 		$this->Nombre = $Nombre;
 		if(!file_exists($this->url . $Nombre))
 			mkdir($this->url . $Nombre, 0777, true);
-		
+		$this->Codigo = self::CrearCodigo();
+		self::EscanearCapitulos();
 	}
 
 	PUBLIC function LimpiarCapitulo()
 	{
-		
+		$this->Capitulo = array();
+	}
+
+	PUBLIC function EscanearCapitulos()
+	{
+			
+	}
+
+	PUBLIC function CrearCodigo()
+	{
+		$scan = scandir($this->url . $Nombre);
+		if(count($scan) == 2){
+			# No se descargo ningun capitulo.
+			
+		}
 	}
 }
